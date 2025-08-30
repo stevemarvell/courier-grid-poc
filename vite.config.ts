f08eccbf5ad3386cd24e3a1077d817dab7079287
Steve Marvell <stevemarvell@crucialtechnical.com>
2025-08-30 20:11:28 +0100

    pages that hopefully  work

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-js
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   const repo = env.VITE_REPO_NAME || '';
   return {
-    base: repo ? `/${repo}/` : '/',       // Dynamic base for GitHub Pages!
+    base: repo ? `/${repo}/` : '/',
     server: { port: 5173 }
   };
 });
